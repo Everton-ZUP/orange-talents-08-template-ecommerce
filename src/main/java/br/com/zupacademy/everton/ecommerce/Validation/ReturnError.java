cf2dfd2cb165504e908b07f636f3f1d0040e3117
Everton-ZUP <everton.fabris@zup.com.br>
2021-08-27 18:22:07 -0300

    Refatoração no retorno de erros

--- src/main/java/br/com/zupacademy/everton/ecommerce/Validation/ReturnError.java
+++ src/main/java/br/com/zupacademy/everton/ecommerce/Validation/ReturnError.java
@@ -1,19 +1,29 @@
 package br.com.zupacademy.everton.ecommerce.Validation;
 
+import java.time.LocalDateTime;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 
 public class ReturnError {
     private List<String> global = new ArrayList<>();
-    private List<FieldErro> fields = new ArrayList<>();
+    private List<HashMap> fields = new ArrayList<>();
+
+  //  private List<FieldErro> fields = new ArrayList<>();
 
     public void AddError (String errorMessage){
         global.add(errorMessage);
     }
 
     public void addErrorField (String campo, Object valor, String mensagem){
-        FieldErro err = new FieldErro(campo,valor,mensagem);
-        fields.add(err);
+
+        HashMap<String,Object> map = new HashMap<>();
+        map.put("Field",campo);
+        map.put("Value",valor);
+        map.put("Message",mensagem);
+        map.put("Time", LocalDateTime.now());
+
+        fields.add(map);
     }
 
     public List<?> getGlobal() {
