ab0b97c1d9f5d0f1f1556b7a88e62217b58ef873
Everton-ZUP <everton.fabris@zup.com.br>
2021-09-06 17:34:41 -0300

    testes e refatoração

--- src/testIntegracoes/java/br/com/zupacademy/everton/ecommerce/UtilsTests.java
+++ src/testIntegracoes/java/br/com/zupacademy/everton/ecommerce/UtilsTests.java
@@ -5,12 +5,15 @@ import br.com.zupacademy.everton.ecommerce.categoria.CategoriaRepository;
 import br.com.zupacademy.everton.ecommerce.compra.Compra;
 import br.com.zupacademy.everton.ecommerce.compra.CompraRepository;
 import br.com.zupacademy.everton.ecommerce.compra.GatewayPagamento;
+import br.com.zupacademy.everton.ecommerce.compra.finalizacao.TransacaoRepository;
 import br.com.zupacademy.everton.ecommerce.produto.CaracteristicaProduto;
 import br.com.zupacademy.everton.ecommerce.produto.Produto;
 import br.com.zupacademy.everton.ecommerce.produto.ProdutoRepository;
 import br.com.zupacademy.everton.ecommerce.usuario.Usuario;
 import br.com.zupacademy.everton.ecommerce.usuario.UsuarioRepository;
+import org.junit.jupiter.api.BeforeEach;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.annotation.DirtiesContext;
@@ -34,6 +37,17 @@ public class UtilsTests {
     UsuarioRepository usuarioRepository;
     @Autowired
     CompraRepository compraRepository;
+    @Autowired
+    TransacaoRepository transacaoRepository;
+
+    @BeforeEach
+    public void limpaBase(){
+        transacaoRepository.deleteAll();
+        compraRepository.deleteAll();
+        produtoRepository.deleteAll();
+        categoriaRepository.deleteAll();
+        usuarioRepository.deleteAll();
+    }
 
     public Categoria criaCategoria(){
         Categoria categoria = new Categoria("Categoria Teste");
